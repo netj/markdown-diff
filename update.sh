236eb809242d1e5f77173277bf15c37f39f9637b
Jaeho Shin <netj@sparcs.org>
2013-11-20 14:54:24 -0800

    Reflected master updates to gh-pages

--- update.sh
+++ update.sh
@@ -10,6 +10,14 @@ set -eu
 #}
 
 mirror-master .
+
+# Fork me at GitHub ribbon
+cat >>README.md <<EOF
+
+<div>
+<a href="https://github.com/netj/markdown-diff" id="github-ribbon"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+</div>
+EOF
 compile-markdown . name="${GitHubRepo#*/}"
 mv -f preview.html index.html
 git add index.html
