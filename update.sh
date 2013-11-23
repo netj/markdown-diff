94ffb37e3a455a325a8fb380d75547384ee3aee7
Jaeho Shin <netj@sparcs.org>
2013-11-22 16:10:06 -0800

    Using latest version of BuildKit's update-gh-pages

--- update.sh
+++ update.sh
@@ -5,8 +5,17 @@ set -eu
 #    google_analytics="UA-...."
 #)
 
-#insert-nav-extras() {
-#    echo '<div class="extra"><a href="http://${GitHubRepo%/*}.github.io/${GitHubRepo#*/}">Home</a></div>'
+#insert-footer() {
+#    echo '&copy; '"$(date +%Y)"' '"$(git config user.name)"'.'
+#}
+
+insert-nav-extras() {
+#    echo '<div class="extra"><a href="http://'"${GitHubRepo%/*}"'.github.io/'"${GitHubRepo#*/}"'">Home</a></div>'
+    echo '<div class="extra"><a href="https://github.com/'"$GitHubRepo"'"><i class="fa fa-github"></i> Source Code</a></div>'
+    echo '<div class="extra"><a href="https://github.com/'"$GitHubRepo"'/issues"><i class="fa fa-bug"></i> Issues</a></div>'
+}
+#insert-nav-footer() {
+#    echo '<a href="http://'"${GitHubRepo%/*}"'.github.io/'"${GitHubRepo#*/}"'">Home</a>'
 #}
 
 mirror-master .
